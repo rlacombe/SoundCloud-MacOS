465a329e3a439deeeb3097a8239a9f5946633c32
Romain Lacombe <romain.lacombe@gmail.com>
2025-02-06 15:54:25 -0800

    Initial Commit

---
+++ SoundCloud/SoundCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundCloudApp.swift
+//  SoundCloud
+//
+//  Created by Romain on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
